f09428b1bcd6cca32be7347c438066b0ff35e910
jllerenac <josellerenacarpio@gmail.com>
2022-08-20 22:43:54 -0500

    Initial Commit

--- URLbuilder.py
+++ URLbuilder.py
@@ -1,12 +1,11 @@
 import requests
 requests.packages.urllib3.disable_warnings()
 from pprint import pprint
-URL = "https://localhost/"
+URL = "http://localhost/"
 
 r = requests.get(URL,verify=False,allow_redirects = False)
 #f = urllib.request.urlopen(URL)
 print(r.status_code)
 pprint(r.headers["Location"])
-
 
 
