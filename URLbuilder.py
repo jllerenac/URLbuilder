f81de18ef4e4a4cd6abfc133c00f8de1fd465c80
jllerenac <josellerenacarpio@gmail.com>
2022-08-20 22:53:21 -0500

    Small change

--- URLbuilder.py
+++ URLbuilder.py
@@ -1,11 +1,16 @@
 import requests
 requests.packages.urllib3.disable_warnings()
 from pprint import pprint
+import argparse
+parser = argparse.ArgumentParser(description='A test program.')
+args = parser.parse_args()
+
 URL = "http://localhost/"
 
-r = requests.get(URL,verify=False,allow_redirects = False)
+r = requests.get(URL,verify=False,allow_redirects = False,timeout=5)
 #f = urllib.request.urlopen(URL)
 print(r.status_code)
-pprint(r.headers["Location"])
+if r.status_code == '200':
+    pprint("[+]"+r.headers["Location"])
 
 
